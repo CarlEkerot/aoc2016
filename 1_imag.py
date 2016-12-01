76353f1e47661d9cc2f565c6e5edd6e06eb841ce
Carl Ekerot <kalle@recordedfuture.com>
2016-12-01 16:48:59 +0100

    One-line first part of day 1

--- 1_imag.py
+++ 1_imag.py
@@ -4,17 +4,12 @@ import re
 def parse():
     with open("1.txt") as f:
         matches = re.findall("(([LR])(\d+)),?\s*", f.read())
-        return map(lambda (a, b, c): (b, int(c)), matches)
+        return map(lambda (a, b, c): ({'R': -1j, 'L': 1j}[b], int(c)), matches)
 
 
-def get_dist(directions):
-    curr_dir = 1  # Start facing north
-    curr_pos = 0
-
-    for (rot, steps) in directions:
-        curr_dir *= {'R': -1j, 'L': 1j}[rot]
-        curr_pos += steps * curr_dir
-    return abs(curr_pos.imag) + abs(curr_pos.real)
+def get_dist(dirs):
+    pos = reduce(lambda (d, p), (r, s): (d * r, p + s * d * r), dirs, (1j, 0))[1]
+    return abs(pos.real) + abs(pos.imag)
 
 
 def get_dist_visited_twice(directions):
@@ -23,7 +18,7 @@ def get_dist_visited_twice(directions):
     visited = {curr_pos: 1}
 
     for (rot, steps) in directions:
-        curr_dir *= {'R': -1j, 'L': 1j}[rot]
+        curr_dir *= rot
         for i in range(1, steps+1):
             curr_pos += curr_dir
             if curr_pos in visited:
@@ -34,4 +29,4 @@ def get_dist_visited_twice(directions):
 
 
 print(get_dist(parse()))
-print(get_dist_visited_twice(parse()))
+print get_dist_visited_twice(parse())
