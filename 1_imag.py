9780b82b46e76e13098238d8954e7b7fac14d6f1
Carl Ekerot <kalle@recordedfuture.com>
2016-12-01 18:21:50 +0100

    Function name fix

--- 1_imag.py
+++ 1_imag.py
@@ -16,11 +16,11 @@ def get_dist(dirs):
     return abs(sum(dirs).real) + abs(sum(dirs).imag)
 
 
-def get_dist_visited_twice2(dirs):
+def get_dist_visited_twice(dirs):
     sums = [sum(dirs[:i+1]) for i in range(len(dirs))]
     pos = [d for i, d in enumerate(sums) if d in sums[:i]][0]
     return abs(pos.real) + abs(pos.imag)
 
 
 print get_dist(parse())
-print get_dist_visited_twice2(parse())
+print get_dist_visited_twice(parse())
