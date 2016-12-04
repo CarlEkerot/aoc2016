93e1e67106528afec0dc86862aefa466e27442c5
Carl Ekerot <kalle@implode.se>
2016-12-04 10:32:34 +0100

    Day 3

---
+++ 3.py
@@ -0,0 +1,16 @@
+def parse():
+    with open("3.txt") as f:
+        return list(map(lambda l: list(map(int, l.split())), f.readlines()))
+
+
+def transpose(sides):
+    return [s for t in [zip(*sides[i:i+3]) for i in range(0, len(sides), 3)] for s in t]
+
+
+def solve(sides):
+    return len(list(filter(lambda s: abs(s[0] - s[1]) < s[2] < s[0] + s[1], map(list, sides))))
+
+
+s = parse()
+print(solve(s))
+print(solve(transpose(s)))
